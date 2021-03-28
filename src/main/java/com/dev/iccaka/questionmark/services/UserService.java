49b12bca280235024f1caef8673ea01deb31f2f6
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-03-28 18:33:36 +0300

    Add '@Service' annotation to 'UserServiceImpl'

--- src/main/java/com/dev/iccaka/questionmark/services/UserService.java
+++ src/main/java/com/dev/iccaka/questionmark/services/UserService.java
@@ -1,8 +1,8 @@
 package com.dev.iccaka.questionmark.services;
 
 import com.dev.iccaka.questionmark.entities.User;
-
 import java.util.List;
+
 
 public interface UserService {
 
--- src/main/java/com/dev/iccaka/questionmark/services/UserServiceImpl.java
+++ src/main/java/com/dev/iccaka/questionmark/services/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.dev.iccaka.questionmark.services;
 
 import com.dev.iccaka.questionmark.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService{
 
     private JpaRepository<User, Long> repository;
