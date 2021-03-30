efe26e6dd36fec0a1bdef736ef83220895ea698d
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-03-30 17:38:31 +0300

    Add getters and setters inside User

--- src/main/java/com/dev/iccaka/questionmark/entities/User.java
+++ src/main/java/com/dev/iccaka/questionmark/entities/User.java
@@ -35,4 +35,20 @@ public class User {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
