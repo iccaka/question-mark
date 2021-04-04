f06cec89c8b0b0f3852af7b00d5e6286b2aca2e1
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-04-04 21:55:45 +0300

    Add registerUser method inside UsersController

--- src/main/java/com/dev/iccaka/questionmark/controllers/UsersController.java
+++ src/main/java/com/dev/iccaka/questionmark/controllers/UsersController.java
@@ -4,10 +4,7 @@ import com.dev.iccaka.questionmark.entities.User;
 import com.dev.iccaka.questionmark.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,5 +30,10 @@ public class UsersController {
         Optional<User> result = userService.findById(id);
 
         return result.isPresent() ? ResponseEntity.ok(result) : ResponseEntity.badRequest().body("User with such ID not found!");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(){
+
     }
 }
