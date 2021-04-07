b06f0b56a0321dcdff96d4339fb04950317c66a0
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-04-07 15:24:20 +0300

    Add Country entity

---
+++ src/main/java/com/dev/iccaka/questionmark/entities/Country.java
@@ -0,0 +1,32 @@
+package com.dev.iccaka.questionmark.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Country(){ }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
