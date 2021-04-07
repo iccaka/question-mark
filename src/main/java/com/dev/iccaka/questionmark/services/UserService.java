a2c5e977d1675aff01434bb1b2f47e1550aa05a8
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-04-07 09:20:00 +0300

    Remove redundant imports

--- src/main/java/com/dev/iccaka/questionmark/services/UserService.java
+++ src/main/java/com/dev/iccaka/questionmark/services/UserService.java
@@ -5,7 +5,6 @@ import com.dev.iccaka.questionmark.entities.User;
 import com.dev.iccaka.questionmark.exceptions.UserAlreadyExistsException;
 import com.dev.iccaka.questionmark.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
