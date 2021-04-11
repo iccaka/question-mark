54751719e22687a66767608235447ef8de33919d
Konstantin Stoyanov <stoyanovkonstantin00@gmail.com>
2021-04-11 17:30:34 +0300

    Build back & front end

--- question-mark-backend/src/main/java/com/dev/iccaka/questionmark/QuestionMarkApplication.java
+++ question-mark-backend/src/main/java/com/dev/iccaka/questionmark/QuestionMarkApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuestionMarkApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionMarkApplication.class, args);
 	}
-
 }
