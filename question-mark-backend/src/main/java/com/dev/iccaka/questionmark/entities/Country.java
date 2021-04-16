0c13181736b67bd6146ae38c3a4beece1ae935aa
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-04-16 13:14:31 +0300

    Add third annotation inside Country

--- question-mark-backend/src/main/java/com/dev/iccaka/questionmark/entities/Country.java
+++ question-mark-backend/src/main/java/com/dev/iccaka/questionmark/entities/Country.java
@@ -1,13 +1,13 @@
 package com.dev.iccaka.questionmark.entities;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter @NoArgsConstructor
 public class Country {
 
     @Id
