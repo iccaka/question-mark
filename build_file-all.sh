4d72796e7bbb11c3f35e3b813f12974d8379b426
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-04-23 22:11:39 +0300

    Modify .sh build file

--- build_file-all.sh
+++ build_file-all.sh
@@ -1,3 +1,3 @@
-(cd question-mark-ui/question-mark/src/app && ng build --prod)
+(cd question-mark-ui/question-mark/src/app && ng build)
 mvn clean install
 cd question-mark-backend/target && java -jar question-mark-backend-0.0.1-SNAPSHOT.jar
