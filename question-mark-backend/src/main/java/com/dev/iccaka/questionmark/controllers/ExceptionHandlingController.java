6d2a3b244c839d31267d267c29fcb70ff956e67a
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-04-23 21:26:19 +0300

    Add ExceptionHandlingController

---
+++ question-mark-backend/src/main/java/com/dev/iccaka/questionmark/controllers/ExceptionHandlingController.java
@@ -0,0 +1,13 @@
+package com.dev.iccaka.questionmark.controllers;
+
+import com.dev.iccaka.questionmark.exceptions.UserAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class ExceptionHandlingController {
+
+    
+}
