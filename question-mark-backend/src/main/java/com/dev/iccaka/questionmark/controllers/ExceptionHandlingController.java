1327f07320fbba3256de9509e570de7657aad249
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2021-04-23 21:28:30 +0300

    Remove imports inside ExceptionHandlingController

--- question-mark-backend/src/main/java/com/dev/iccaka/questionmark/controllers/ExceptionHandlingController.java
+++ question-mark-backend/src/main/java/com/dev/iccaka/questionmark/controllers/ExceptionHandlingController.java
@@ -1,10 +1,6 @@
 package com.dev.iccaka.questionmark.controllers;
 
-import com.dev.iccaka.questionmark.exceptions.UserAlreadyExistsException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ExceptionHandlingController {
